6dd22ff0e901876bed9b2c77244c0bcbaf6b5332
DinoKnezevic1 <knezevic7p@gmail.com>
2023-01-04 23:27:40 +0100

    Created service item

--- app/src/main/java/com/dinoknezevic/barbershopreservation/ui/component/ServiceItem.kt
+++ app/src/main/java/com/dinoknezevic/barbershopreservation/ui/component/ServiceItem.kt
@@ -2,24 +2,24 @@ package com.dinoknezevic.barbershopreservation.ui.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.dinoknezevic.barbershopreservation.R
-import com.dinoknezevic.barbershopreservation.ui.theme.DarkGrey
-import com.dinoknezevic.barbershopreservation.ui.theme.LightGrey
-import com.dinoknezevic.barbershopreservation.ui.theme.dimensions
-import com.dinoknezevic.barbershopreservation.ui.theme.spacing
+import com.dinoknezevic.barbershopreservation.ui.theme.*
 
 data class ServiceItemViewState(
     val imagePath: Int,
@@ -31,19 +31,26 @@ data class ServiceItemViewState(
 @Composable
 fun ServiceItem(
     modifier: Modifier = Modifier,
-    serviceItemViewState: ServiceItemViewState
+    serviceItemViewState: ServiceItemViewState,
+    onClick:()->Unit
 ) {
     Row(
-        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
-            .padding(horizontal = MaterialTheme.spacing.large)
+            .padding(horizontal = MaterialTheme.spacing.large)//vertical only for preview
             .fillMaxWidth()
-            .background(color = LightGrey, shape = RoundedCornerShape(MaterialTheme.spacing.small))
+            .background(
+                color = LightGrey,
+                shape = RoundedCornerShape(MaterialTheme.spacing.medium)
+            )
+            .clickable { onClick() }//string or id(int) of the service?
     ) {
         Box(
             modifier = modifier
-                .weight(1f)
+                .weight(2f)
                 .heightIn(min=MaterialTheme.dimensions.ServiceItemMinHeight)
+                .padding(MaterialTheme.spacing.small)
         ) {
             Image(
                 imageVector = ImageVector.vectorResource(id = serviceItemViewState.imagePath),
@@ -55,52 +62,45 @@ fun ServiceItem(
             modifier = modifier
                 .weight(4f)
                 .heightIn(min=MaterialTheme.dimensions.ServiceItemMinHeight)
+                .padding(MaterialTheme.spacing.small)
         ) {
             ServiceNameDescription(serviceItemViewState)
         }
         Box(
             modifier = modifier
                 .weight(2f)
-                .heightIn(min=MaterialTheme.dimensions.ServiceItemMinHeight)
+                .padding(MaterialTheme.spacing.small)
         ) {
-            ServicePriceReserve(serviceItemViewState)
+            Text(
+                text = serviceItemViewState.price,
+                fontSize= 18.sp,
+                fontWeight= FontWeight.Bold,
+                color = LightOrange,
+                textAlign = TextAlign.Center
+            )
         }
-    }
-}
-
-@Composable
-fun ServicePriceReserve(serviceItemViewState: ServiceItemViewState) {
-    Column(
-        modifier = Modifier
-    ) {
-        Image(
-            imageVector = ImageVector.vectorResource(id = R.drawable.ic_btn_plus),
-            contentDescription = null,
-            contentScale = ContentScale.Crop
-        )
-        Text(
-            text = serviceItemViewState.price,
-            style = MaterialTheme.typography.h3,
-            color = Color.Black
-        )
     }
 }
 
 @Composable
 fun ServiceNameDescription(serviceItemViewState: ServiceItemViewState) {
     Column(
-        modifier = Modifier
+        modifier = Modifier,
+        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.extraSmall),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = serviceItemViewState.name,
             color = Color.Black,
-            style = MaterialTheme.typography.h3
-            //fontSize = MaterialTheme.typography.h3.fontSize,
+            fontSize = 18.sp,
+            fontWeight= FontWeight.Bold,
+            textAlign = TextAlign.Center
         )
         Text(
             text = serviceItemViewState.description,
             color = DarkGrey,
-            fontSize = 16.sp
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center
         )
     }
 }
@@ -117,7 +117,7 @@ private fun ServiceItemPreview(
                 id = R.string.modern_haircut
             ),
             description = "Maaa brtee fade najjaca stvar ikad predobro je vjeruj mi",
-            price = "40.55"
-        )
+            price = "7.65$"
+        ), onClick = {}
     )
 }
